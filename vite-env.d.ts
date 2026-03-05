18cc31c2e9364405775ff3fc21dd23311e2863b9
Tech and Tonic <aaroncrume@gmail.com>
2026-03-05 08:16:07 -0800

    Update API_KEY type in vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -3,7 +3,7 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      API_KEY: string;
+      API_KEY:AIzaSyBZC30bkrI6gzoXZzmd5NfS4w_HoeXZu1A string;
       [key: string]: string | undefined;
     }
   }
